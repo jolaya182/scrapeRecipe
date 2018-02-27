b8d889fbd0c2c2720dec545453351c21df8619f5
javier olayis <jolaya182@gmail.com>
2018-02-27 17:11:18 -0500

    addedMultipleTabsOpen

--- index.js
+++ index.js
@@ -1,10 +1,58 @@
-let clearbit = require('clearbit')("sk_f57934b45e4df7041c759337f52409f3");
 let fs = require("fs");
-const puppeteer = require('puppeteer');
 const readline = require('readline');
+let clearbit = require('clearbit')("sk_f57934b45e4df7041c759337f52409f3");
+const puppeteer = require('puppeteer');
 
-let textCsv = fs.readFileSync("C:/scrape/testScrape.csv", 'utf8');
+let textCsv = fs.readFileSync("C:/scrape/testScrape.csv", 'utf8').split("\r\n");
+///should work for an angellist, hired, linkedin, buildnyc, stackoverflow, workable,  vettery recipe 
+
+//take away the last line
+textCsv.splice(textCsv.length-1);
 console.log(textCsv);
+
+(async () => {
+const browser = await puppeteer.launch({headless: false}); // default is true
+  for(let i = 0; i < textCsv.length; i += 1){
+    // const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto(textCsv[i]);
+    // console.log( "page url", page.url());
+
+    // await browser.close();
+  }
+  let pages = await browser.pages();
+  let count = 0;
+  while(count < textCsv.length){
+    pages = await browser.pages();
+    count = pages.length;
+  }
+  console.log(pages.length);
+  let data = [];
+  let s = "";
+  let currDate = "2/27/2018"
+  let url = ""
+  let jobTitle = "";
+  let companyName = "";
+  let skillsR = "";
+  let req = "";
+  let contactPerson = "";
+  let contactTitle = "";
+  let emailAddress = "";
+  let socialFound = "";
+  let whatIFindInteresting = "";
+  let product1 = "";
+  let product2 = "";
+  let responded = "";
+  let subjectTitle = "";
+  let rejected = "";
+  let note = "";
+  for(let i = 0; i < pages.length; i += 1){
+
+    s = 
+    //reset s = ""
+  }
+  
+})();
 
 const rl = readline.createInterface({
   input : fs.createReadStream("C:/scrape/testScrape.csv"),
@@ -17,26 +65,21 @@ rl.on('line',(line)=>{
   // console.log(`received:${line}`);
   jobApplicationList.push(line);
 
-  (async () => {
-    const browser = await puppeteer.launch({headless: false}); // default is true
-    // const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    await page.goto(line);
-    
-    // Get the "viewport" of the page, as reported by the page.
-    // const dimensions = await page.evaluate(() => {
-    //   return {
-    //     width: document.documentElement.clientWidth,
-    //     height: document.documentElement.clientHeight,
-    //     deviceScaleFactor: window.devicePixelRatio
-    //   };
-    // });
+  // (async () => {
+  //   const browser = await puppeteer.launch({headless: false}); // default is true
+  //   // const browser = await puppeteer.launch();
+  //   const page = await browser.newPage();
+  //   await page.goto(line);
   
-    // console.log('Dimensions:', dimensions);
-  
-    // await browser.close();
-  })();
+  //   // await browser.close();
+  // })();
 
+  // puppeteer.launch().then(async browser => {
+  //   const page = await browser.newPage();
+  //   await page.goto('https://example.com');
+  //   await page.screenshot({path: 'screenshot.png'});
+  //   await browser.close();
+  // });
   
 });
 
